ece0e93506311a6af75af1313186638190f0eab3
Ian Huang <imyxhuang@gmail.com>
2016-05-22 13:54:05 -0700

    Responsive font

--- sidebar.js
+++ sidebar.js
@@ -2,6 +2,18 @@ $(document).ready(function() {
 	sidebarOut = true;
 	retract = true;
 	done = false;
+    firstResize = false;
+    if (firstResize = false) {
+        document.getElementsByTagName("body")[0].style[ "font-size" ] = document.body.clientWidth*(14/1366) + "px";
+        document.getElementsByTagName("h1")[0].style[ "font-size" ] = document.body.clientWidth*(48/1366) + "px";
+        document.getElementsByTagName("h2")[0].style[ "font-size" ] = document.body.clientWidth*(36/1366) + "px";
+        document.getElementsByTagName("h3")[0].style[ "font-size" ] = document.body.clientWidth*(24/1366) + "px";
+        document.getElementsByTagName("h4")[0].style[ "font-size" ] = document.body.clientWidth*(18/1366) + "px";
+        document.getElementsByTagName(".main-subheading")[0].style[ "font-size" ] = document.body.clientWidth*(18/1366) + "px";
+        document.getElementsByTagName(".sidebar-item")[0].style[ "height" ] = document.body.clientWidth*(70/1366) + "px";
+        document.getElementsByTagName(".sidebar-item")[0].style[ "line-height" ] = document.body.clientWidth*(70/1366) + "px";
+        firstResize = true;
+    }
 	$(".sidebar").hover(function() {
 		retract = false;
 		$(".sidebar").mouseleave(function() {
@@ -57,5 +69,13 @@ $(document).ready(function() {
 		} else {
 			$(".sidebar").animate({right: '-' + ($(".sidebar-item").width() + 7) + 'px'});
 		}
+        document.getElementsByTagName("body")[0].style[ "font-size" ] = document.body.clientWidth*(14/1366) + "px";
+        document.getElementsByTagName("h1")[0].style[ "font-size" ] = document.body.clientWidth*(48/1366) + "px";
+        document.getElementsByTagName("h2")[0].style[ "font-size" ] = document.body.clientWidth*(36/1366) + "px";
+        document.getElementsByTagName("h3")[0].style[ "font-size" ] = document.body.clientWidth*(24/1366) + "px";
+        document.getElementsByTagName("h4")[0].style[ "font-size" ] = document.body.clientWidth*(18/1366) + "px";
+
+        document.getElementsByTagName(".sidebar-item")[0].style[ "height" ] = document.body.clientWidth*(70/1366) + "px";
+        document.getElementsByTagName(".sidebar-item")[0].style[ "line-height" ] = document.body.clientWidth*(70/1366) + "px";
 	});
 });
